f9242dc8c76586b91c1e6eca74786569c05de4cb
Caleb <croffca@mail.gvsu.edu>
2025-02-05 09:59:35 -0500

    Caleb: Worked on evolve method

--- card.py
+++ card.py
@@ -1,20 +1,34 @@
 class Card:
-    def __init__(self, name, type, hp, stage, weakness, retreat):
+    def __init__(self, name, type, hp, stage, weakness, retreat_cost):
         self.name = name
         self.type = type
         self.hp = hp
         self.stage = stage
         self.weakness = weakness
-        self.retreat = retreat
+        self.retreat_cost = retreat_cost
 
     def attack(self):
         pass
 
-    def evolve(self):
-        pass
+    def evolve(self, new):
+        print(self.name)
+        if self.stage == "basic" and new.stage == "stage 1":
+            self = new
+            print(self.name)
+        elif self.stage == "stage 1" and new.stage == "stage 2":
+            self = new
 
     def attach_energy(self):
         pass
 
     def retreat(self):
         pass
+
+
+pikachu = Card("Pikachu", "electric", 70, "basic", "ground", 1)
+
+raichu = Card("Raichu", "electric", 110, "stage 1", "ground", 1)
+
+pikachu.evolve(raichu)
+
+
