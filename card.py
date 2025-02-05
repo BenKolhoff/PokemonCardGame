e2e70c58e97930190973e6cc8093102a1af35538
Caleb <croffca@mail.gvsu.edu>
2025-02-05 10:26:44 -0500

    Caleb: Added error handling to evolve

--- card.py
+++ card.py
@@ -1,22 +1,29 @@
 class Card:
-    def __init__(self, name, type, hp, stage, weakness, retreat_cost):
+    def __init__(self, name, type, hp, stage, weakness, retreat_cost, evolves_from):
         self.name = name
         self.type = type
         self.hp = hp
         self.stage = stage
         self.weakness = weakness
         self.retreat_cost = retreat_cost
+        self.evolves_from = evolves_from
 
     def attack(self):
         pass
 
     def evolve(self, new):
-        print(self.name)
         if self.stage == "basic" and new.stage == "stage 1":
-            self = new
-            print(self.name)
+            if new.evolves_from == self.name:
+                print(self.name, "evolved to", new.name)
+                self = new
+            else:
+                print(self.name, "doesnt evolve from", new.name)
         elif self.stage == "stage 1" and new.stage == "stage 2":
+            print(self.name, "evolved to", new.name)
             self = new
+        else:
+            print(self.stage, "can't evolve to a", new.stage)
+        
 
     def attach_energy(self):
         pass
@@ -25,9 +32,9 @@ class Card:
         pass
 
 
-pikachu = Card("Pikachu", "electric", 70, "basic", "ground", 1)
-
-raichu = Card("Raichu", "electric", 110, "stage 1", "ground", 1)
+pikachu = Card("Pikachu", "electric", 70, "stage 1", "ground", 1, None)
+raichu = Card("Raichu", "electric", 110, "basic", "ground", 1, "Pikachu")
+charmeleon = Card("Charmeleon", "fire", 110, "stage 1", "water", 2, "Charmander")
 
 pikachu.evolve(raichu)
 
