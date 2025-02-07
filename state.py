b4ce270859aea957106376b88745d40a7cd33e53
BenKolhoff <benkolhoff@gmail.com>
2025-02-07 11:10:46 -0500

    Added change_player method to state class

--- state.py
+++ state.py
@@ -5,6 +5,9 @@ class State:
         self.playerA = Player()
         self.playerB = Player()
         self.current_player = self.playerA if current_player is None else current_player
-        
 
-    
+    '''
+    Changes the current player to the opposite player.
+    '''
+    def change_player(self):
+        self.current_player = self.playerA if self.current_player == self.playerB else self.playerB
