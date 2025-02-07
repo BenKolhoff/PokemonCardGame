5178570142c83c03651283c67303c65b2d6ed7e5
BenKolhoff <benkolhoff@gmail.com>
2025-02-07 11:04:18 -0500

    Added bones of player class

---
+++ player.py
@@ -0,0 +1,11 @@
+class Player:
+    def __init__(self, active_card=None, benched_cards=[], hand=[], deck=[]):
+        self.active_card = active_card
+        self.benched_cards = benched_cards
+        self.hand = hand
+        self.deck = deck
+    
+    # Methods to implement:
+    # set_active_card
+    # bench_card
+    # draw_card
