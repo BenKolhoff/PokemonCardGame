e362dcfb256bd78d94092fd142748c855a050b32
BenKolhoff <benkolhoff@gmail.com>
2025-02-07 11:07:52 -0500

    Changed state class to implement player class

--- state.py
+++ state.py
@@ -1,6 +1,10 @@
+from player import Player
+
 class State:
-    def __init__(self, current_player):
-        self.current_player = current_player
+    def __init__(self, current_player=None):
+        self.playerA = Player()
+        self.playerB = Player()
+        self.current_player = self.playerA if current_player is None else current_player
         
 
     
