376be75513d8350e1e316dc41d75bdeb2a8da043
Caleb <croffca@mail.gvsu.edu>
2025-02-12 10:22:20 -0500

    Caleb: Created unittest file

---
+++ tests.py
@@ -0,0 +1,26 @@
+import unittest
+
+from card import Card
+from move import Move
+from player import Player
+from state import State
+
+
+class cardTest(unittest.TestCase):
+    def test_init(self):
+        pass
+
+    def test_attack(self):
+        pass
+
+    def test_evolve(self):
+        pass
+
+    def test_attach_energy(self):
+        pass
+
+    def test_retreat(self):
+        pass
+
+    def test_discard(self):
+        pass
