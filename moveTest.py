85821f52a5ab824c19473e14475dd462ce79e5be
Caleb <croffca@mail.gvsu.edu>
2025-02-12 11:06:35 -0500

    Caleb: Added more test files

---
+++ moveTest.py
@@ -0,0 +1,6 @@
+import unittest
+from move import Move
+
+class moveTest(unittest.TestCase):
+    def test_init(self):
+        pass
---
+++ playerTest.py
@@ -0,0 +1,12 @@
+import unittest
+from player import Player
+
+class testPlayer(unittest.TestCase):
+    def test_init(self):
+        pass
+
+    def test_set_active_card(self):
+        pass
+    
+    def test_bench_card(self):
+        pass
