1e0dafe9964c00080e38faf42ffb3f1c151ad881
BenKolhoff <benkolhoff@gmail.com>
2025-02-19 14:21:03 -0500

    Added documentation to the code on all methods that didn't have it.

--- card.py
+++ card.py
@@ -13,12 +13,18 @@ class Card:
         self.evolves_from = evolves_from if type(evolves_from) == str or evolves_from == None else None
         self.energy = 0
 
+    '''
+    Attacks another card (lowers their HP by the amount of damage caused by the used move).
+    '''
     def attack(self, target, move):
         target.hp -= move.damage
         print(self.name, "used", move.name, "for", move.damage, "damage")
         print(target.name, "HP is now", target.hp)
         print()
 
+    '''
+    Evolves the card into the specified evolution.
+    '''
     def evolve(self, new):
         if self.stage == "basic" and new.stage == "stage 1":
             if new.evolves_from == self.name:
@@ -32,10 +38,15 @@ class Card:
         else:
             print(self.stage, "can't evolve to a", new.stage)
         
-
+    '''
+    Attaches energy to this card (increases energy by one).
+    '''
     def attach_energy(self):
         self.energy += 1
 
+    '''
+    Moves the card from being "active" to the bench.
+    '''
     def retreat(self):
         if self.energy >= self.retreat_cost:
             # move from active to bench
--- player.py
+++ player.py
@@ -6,11 +6,6 @@ class Player:
         self.deck = deck if type(deck) == list else []
         self.discard = []
         self.points = 0
-    
-    # Methods to implement:
-    # set_active_card
-    # bench_card
-    # draw_card
 
     def set_active_card(self, card):
         self.active_card = card
@@ -29,9 +24,15 @@ class Player:
         elif card in self.benched_cards:
             self.benched_cards.remove(card)
     
+    '''
+    Sets the deck of the player instance to the specified deck.
+    '''
     def set_deck(self, deck):
         self.deck = deck
     
+    '''
+    Draws a card from the player's deck.
+    '''
     def draw_card(self):
         if self.deck[0] == None:
             print("Cannot draw, your deck is empty")
@@ -41,6 +42,9 @@ class Player:
         self.hand.append(deck_top)
         self.deck.remove(deck_top)
 
+    '''
+    Prints the hand in a clean format.
+    '''
     def print_hand(self):
         for card in self.hand:
             print(card['Name'], end=' ')
