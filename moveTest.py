c823ae7c52a516bb9ffd156b328df332a0eeff14
Caleb <croffca@mail.gvsu.edu>
2025-02-22 12:30:11 -0500

    Caleb: Added unit tests for move class

--- moveTest.py
+++ moveTest.py
@@ -1,10 +1,25 @@
 import unittest
 from move import Move
 
-class moveTest(unittest.TestCase):
-    def test_init(self):
-        pass
-
+class TestMove(unittest.TestCase):
+    def setUp(self):
+        self.move1 = Move("Tackle", 20, 1)
+        self.move2 = Move("Flamethrower", 90, 3)
+    
+    def test_initialization(self):
+        self.assertEqual(self.move1.name, "Tackle")
+        self.assertEqual(self.move1.damage, 20)
+        self.assertEqual(self.move1.cost, 1)
+        
+        self.assertEqual(self.move2.name, "Flamethrower")
+        self.assertEqual(self.move2.damage, 90)
+        self.assertEqual(self.move2.cost, 3)
+    
+    def test_invalid_initialization(self):
+        move = Move(123, "invalid", "wrong")
+        self.assertEqual(move.name, "")
+        self.assertEqual(move.damage, 0)
+        self.assertEqual(move.cost, 0)
 
 if __name__ == "__main__":
-    unittest.main(verbosity=2)
+    unittest.main()
